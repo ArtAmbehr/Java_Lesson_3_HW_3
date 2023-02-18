3d48ac18817cd50af6f0fe9caac286511e2a7be6
Art <art-75@yandex.ru>
2023-02-18 11:04:39 +0300

    mergeSort

---
+++ Task_3.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+public class Task_3 {
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buffer2 = new int[sortArr.length];
+        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
+        return result;
+    }
