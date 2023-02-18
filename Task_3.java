ca2844874d95a2a114a0699a847fb0deeaa47d27
Art <art-75@yandex.ru>
2023-02-18 11:07:54 +0300

    Merge

--- Task_3.java
+++ Task_3.java
@@ -17,4 +17,22 @@ public class Task_3 {
         int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
         int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
 
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2]
+                    ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+
+
 
